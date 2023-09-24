7214f0ae3fbe99c8d9ccd6621868a4ad8a87af9f
SangWonLee <tkddnjs0687@naver.com>
2023-09-24 22:17:36 +0900

    feat: Loading Spinner 컴포넌트 제작

---
+++ src/components/common/spinner/index.ts
@@ -0,0 +1,1 @@
+export { default as LoadingSpinnner } from './loading/LoadingSpinner';
---
+++ src/components/common/spinner/loading/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+
+// TODO: colorType 수정
+interface LoadingSpinnerProps {
+  colorType?: 'black' | 'white' | 'brown';
+}
+
+const LoadingSpinner = ({ colorType = 'white' }: LoadingSpinnerProps) => {
+  return <ClipLoader aria-label="loading..." color={colorType} size={10} />;
+};
+
+export default LoadingSpinner;
