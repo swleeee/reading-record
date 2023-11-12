12180a846d07f6c1a0cd68ef9e6da6b43689bb93
SangWonLee <tkddnjs0687@naver.com>
2023-11-12 15:06:23 +0900

    chore: 호스트 및 포트 설정

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,10 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
