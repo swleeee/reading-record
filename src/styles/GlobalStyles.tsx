ce6ba9cd1f2ee5c8938e3e67f980fbbf0effc8ec
SangWonLee <tkddnjs0687@naver.com>
2023-11-12 21:58:28 +0900

    style: html, body css 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,17 +1,24 @@
 // src/styles/GlobalStyles.tsx
 import React from 'react';
 import { Global } from '@emotion/react';
-import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro';
+import tw, { css, GlobalStyles as BaseStyles } from 'twin.macro';
 
 import './font.css';
 
-const customStyles = css({
-  body: {
-    WebkitTapHighlightColor: theme`colors.purple.500`,
-    ...tw`antialiased`,
-  },
-  button: tw`p-0 border-0`,
-});
+const customStyles = css`
+  *,
+  *::before,
+  *::after {
+    ${tw`m-0 p-0 box-border`}
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    ${tw`min-h-screen`}
+  }
+`;
+
 const GlobalStyles = () => (
   <>
     <BaseStyles />
