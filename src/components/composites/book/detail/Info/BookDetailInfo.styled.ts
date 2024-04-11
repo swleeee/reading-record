2ada5ccdb1589ca59c50dd6cba1171c8db9634bd
SangWonLee <tkddnjs0687@naver.com>
2024-04-11 14:00:38 +0900

    feat: 도서 상세 정보 스크린 제작 (태블릿, 데스크탑 해상도) (#11)

--- src/components/composites/book/detail/Info/BookDetailInfo.styled.ts
+++ src/components/composites/book/detail/Info/BookDetailInfo.styled.ts
@@ -2,9 +2,9 @@ import tw, { css, styled } from 'twin.macro';
 
 export const BookInfoWrapper = tw.section`w-full h-full rounded-[4px] px-[4px]`;
 
-export const BookSubInfoList = tw.dl`relative flex gap-x-[12px] mt-[-25px] mb-[24px] px-[24px]`;
+export const BookSubInfoList = tw.dl`relative flex justify-center gap-x-[12px] mt-[-25px] mb-[24px] px-[24px] desktop:mt-[-32px]`;
 
-export const BookSubInfoWrapper = tw.div`flex-1 border border-brown100 rounded-[4px] shadow-light_sm overflow-hidden`;
+export const BookSubInfoWrapper = tw.div`flex-1 max-w-[258px] border border-brown100 rounded-[4px] shadow-light_sm overflow-hidden`;
 
 export const BookSubInfoTitle = tw.dt`py-[4px] m-body-m13 text-center bg-white tablet:t-body-m13 desktop:d-body-m15`;
 
@@ -12,7 +12,7 @@ export const BookSubInfoDescription = tw.dt`h-full py-[6px] m-body-r12 text-cent
 
 export const BookThumbnailSection = styled.section<{ src: string }>`
   ${({ src }) => css`
-    ${tw`relative flex justify-center items-center w-full h-[300px] before:(content-[''] absolute top-0 left-0 right-0 bottom-0 bg-no-repeat bg-cover bg-blend-multiply opacity-70)`}
+    ${tw`relative flex justify-center items-center w-full h-[300px] before:(content-[''] absolute top-0 left-0 right-0 bottom-0 bg-no-repeat bg-cover bg-blend-multiply opacity-70) tablet:h-[500px] desktop:h-[660px]`}
 
     &::before {
       // FIXME: tw를 이용한 방법 실패하여 하기와 같이 작성. 추후 수정 필요
@@ -34,11 +34,11 @@ export const BookThumbnailSection = styled.section<{ src: string }>`
   `}
 `;
 
-export const BookThumbnail = tw.img`relative w-[144px] h-[216px] rounded-[4px] shadow-light_lg`;
+export const BookThumbnail = tw.img`relative w-[144px] h-[216px] rounded-[4px] object-fill shadow-light_lg tablet:(w-[178px] h-[252px]) desktop:(w-[198px] h-[286px])`;
 
 export const BookTitle = tw.span`m-body-m14 tablet:t-body-m14 desktop:d-body-m16`;
 
-export const BookInfoList = tw.dl`grid grid-cols-[minmax(80px, auto) 1fr] gap-y-[4px] rounded-[4px] mx-[12px] px-[12px] py-[10px] bg-brown50`;
+export const BookInfoList = tw.dl`grid grid-cols-[minmax(80px, auto) 1fr] gap-y-[4px] max-w-[800px] rounded-[4px] mx-[24px] px-[12px] py-[10px] bg-brown50 tablet:mx-auto`;
 
 export const BookInfoTitle = tw.dt`m-body-m13 tablet:t-body-m13 desktop:d-body-m15`;
 
--- src/components/composites/book/detail/Info/BookDetailInfo.tsx
+++ src/components/composites/book/detail/Info/BookDetailInfo.tsx
@@ -1,22 +1,57 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
 
-import { deviceState } from '@/stores';
 import type { GetBooksServerModel } from '@/types';
-import BookDetailInfoDesktop from './desktop/BookDetailInfoDesktop';
-import BookDetailInfoMobile from './mobile/BookDetailInfoMobile';
+import * as S from './BookDetailInfo.styled';
 
 interface BookInfoContentProps {
   book: GetBooksServerModel['documents'][number];
 }
 
 const BookDetailInfo = ({ book }: BookInfoContentProps) => {
-  const device = useRecoilValue(deviceState);
-
-  return device === 'mobile' ? (
-    <BookDetailInfoMobile book={book} />
-  ) : (
-    <BookDetailInfoDesktop book={book} />
+  return (
+    <section>
+      <S.BookThumbnailSection src={book.thumbnail}>
+        <S.BookThumbnail src={book.thumbnail} alt="book thumbnail" />
+      </S.BookThumbnailSection>
+      <S.BookInfoWrapper>
+        <S.BookSubInfoList>
+          <S.BookSubInfoWrapper>
+            <S.BookSubInfoTitle>평점</S.BookSubInfoTitle>
+            <S.BookSubInfoDescription>4.5 / 5</S.BookSubInfoDescription>
+          </S.BookSubInfoWrapper>
+          <S.BookSubInfoWrapper>
+            <S.BookSubInfoTitle>읽기 완료 수</S.BookSubInfoTitle>
+            <S.BookSubInfoDescription>123k</S.BookSubInfoDescription>
+          </S.BookSubInfoWrapper>
+          <S.BookSubInfoWrapper>
+            <S.BookSubInfoTitle>읽기 상태</S.BookSubInfoTitle>
+            <S.BookSubInfoDescription>읽지 않음</S.BookSubInfoDescription>
+          </S.BookSubInfoWrapper>
+        </S.BookSubInfoList>
+        <S.BookInfoList>
+          <S.BookInfoTitle>책 제목</S.BookInfoTitle>
+          <S.BookInfoDescription>{book.title}</S.BookInfoDescription>
+          <S.BookInfoTitle>출간날짜</S.BookInfoTitle>
+          <S.BookInfoDescription>
+            {book.datetime.split('T')[0]}
+          </S.BookInfoDescription>
+          <S.BookInfoTitle>ISBN</S.BookInfoTitle>
+          <S.BookInfoDescription>{book.isbn}</S.BookInfoDescription>
+          <S.BookInfoTitle>저자</S.BookInfoTitle>
+          <S.BookInfoDescription>
+            {book.authors.join(', ')}
+          </S.BookInfoDescription>
+          <S.BookInfoTitle>번역자</S.BookInfoTitle>
+          <S.BookInfoDescription>
+            {book.translators.join(', ')}
+          </S.BookInfoDescription>
+          <S.BookInfoTitle>출판사</S.BookInfoTitle>
+          <S.BookInfoDescription>{book.publisher}</S.BookInfoDescription>
+          <S.BookInfoTitle>상세 내용</S.BookInfoTitle>
+          <S.BookInfoDescription>{book.contents}</S.BookInfoDescription>
+        </S.BookInfoList>
+      </S.BookInfoWrapper>
+    </section>
   );
 };
 
