d921a6aee4127ddade834096d9946a3bfbaaa583
SangWonLee <tkddnjs0687@naver.com>
2024-04-12 15:01:51 +0900

    style: Textarea 기본 스타일 수정 (resize, outline, scroll 등) (#14)

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -35,6 +35,25 @@ const customStyles = css`
   a {
     -webkit-tap-highlight-color: transparent;
   }
+
+  textarea {
+    resize: none;
+    outline: 0;
+
+    // NOTE: 스크롤 바 전체에 대한 스타일 지정
+    ::-webkit-scrollbar {
+      ${tw`w-[4px] h-[4px] tablet:(w-[8px] h-[8px])`}
+    }
+
+    // NOTE: 스크롤 바의 색상 지정 */
+    ::-webkit-scrollbar-thumb {
+      ${tw`rounded-full bg-brown300`}
+
+      &:hover {
+        ${tw`bg-brown500`}
+      }
+    }
+  }
 `;
 
 const GlobalStyles = () => (
