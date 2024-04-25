2a0ceb893f0a7bd687c6255a0aa2e32efb086aa4
SangWonLee <tkddnjs0687@naver.com>
2024-04-25 22:57:19 +0900

    refactor: 이용약관 목차 내 태그 변경 (#21)

--- src/components/composites/terms/articles/20240424/index/TermsIndex.styled.ts
+++ src/components/composites/terms/articles/20240424/index/TermsIndex.styled.ts
@@ -4,7 +4,7 @@ export const Container = tw.section``;
 
 export const MainWrapper = tw.ol`block max-w-[1024px] mx-auto mb-[40px] border border-gray300 rounded-[4px] p-[24px] [column-count: 1] tablet:(mb-[60px] py-[28px] [column-count: 2]) desktop:(mb-[80px] py-[32px] [column-count: 3])`;
 
-export const Li = tw.li`m-body-r12 list-none not-last-of-type:mb-[24px] tablet:(t-body-r12 not-last-of-type:mb-[28px]) desktop:(d-body-r14 not-last-of-type:mb-[32px])`;
+export const MainListItem = tw.li`m-body-r12 list-none not-last-of-type:mb-[24px] tablet:(t-body-r12 not-last-of-type:mb-[28px]) desktop:(d-body-r14 not-last-of-type:mb-[32px])`;
 
 export const MainTextLink = tw.a`inline-block mb-[6px] m-headline-4 tablet:t-headline-4 desktop:d-headline-4`;
 
