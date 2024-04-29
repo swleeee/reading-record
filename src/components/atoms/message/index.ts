aaecd452b7878036cae2095cc2db79099885e313
SangWonLee <tkddnjs0687@naver.com>
2024-04-29 15:38:57 +0900

    feat: 통과 메시지 컴포넌트 제작 (#19)

--- src/components/atoms/message/index.ts
+++ src/components/atoms/message/index.ts
@@ -1,1 +1,2 @@
 export { default as ErrorMessage } from './error/ErrorMessage';
+export { default as PassMessage } from './pass/PassMessage';
---
+++ src/components/atoms/message/pass/PassMessage.styled.ts
@@ -0,0 +1,3 @@
+import tw from 'twin.macro';
+
+export const PassMessage = tw.p`m-body-r13 text-blue200 tablet:t-body-r13 desktop:d-body-r15`;
