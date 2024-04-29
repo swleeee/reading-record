dbaec0099f438ef7f6b391fac7577dcfc251a2a6
SangWonLee <tkddnjs0687@naver.com>
2024-04-29 15:39:42 +0900

    feat: LabelContent pass props 추가 및 ErrorMessage 컴포넌트 활용하도록 수정 (#19)

--- src/components/atoms/message/error/ErrorMessage.tsx
+++ src/components/atoms/message/error/ErrorMessage.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import * as S from './ErrorMessage.styled';
 
 interface ErrorMessageProps {
+  className?: string;
   message: string;
 }
 
-const ErrorMessage = ({ message }: ErrorMessageProps) => {
-  return <S.ErrorMessage>{message}</S.ErrorMessage>;
+const ErrorMessage = ({ className, message }: ErrorMessageProps) => {
+  return <S.ErrorMessage className={className}>{message}</S.ErrorMessage>;
 };
 
 export default ErrorMessage;
