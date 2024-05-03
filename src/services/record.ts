2f2ce5c1743dc75832ad47f0d6c30000ba65bfe7
SangWonLee <tkddnjs0687@naver.com>
2024-05-03 14:48:44 +0900

    feat: 유저 독서 감상문 좋아요 추가/삭제 낙관적 업데이트 적용 (#29)

--- src/services/record.ts
+++ src/services/record.ts
@@ -16,6 +16,7 @@ import type {
   GetBookRecordQueryModel,
   GetBookUserRecordsQueryModel,
   GetTotalLikeForRecordQueryModel,
+  GetTotalLikeForRecordServerModel,
   UpdateBookRecordQueryModel,
 } from '@/types';
 import queryClient from './queryClient';
@@ -83,7 +84,59 @@ export const useCreateLikeForRecord = () => {
   return useMutation({
     mutationFn: (req: CreateLikeForRecordQueryModel) =>
       createLikeForRecordAPI(req),
-    onSuccess: (_, variables) => {
+    onMutate: async (variables) => {
+      const queryKey = bookRecordKeys.userRecordLike(
+        variables.isbn,
+        variables.recordId,
+      );
+
+      // NOTE: 쿼리 취소
+      await queryClient.cancelQueries({ queryKey });
+
+      /*       
+        NOTE: getQueryData 함수
+              - 쿼리의 캐싱된 데이터를 가져오는 데 사용할 수 있는 동기 함수
+              - 쿼리가 존재하지 않으면 undefined를 반환
+      */
+      const oldData: [GetTotalLikeForRecordServerModel] | undefined =
+        queryClient.getQueryData(queryKey);
+
+      if (oldData && oldData.length > 0) {
+        const newData = [
+          {
+            isliked: !oldData[0].isliked,
+            count: oldData[0].isliked
+              ? oldData[0].count - 1
+              : oldData[0].count + 1,
+          },
+        ];
+
+        /* 
+          NOTE: setQueryData 함수
+                - 쿼리의 캐시 된 데이터를 즉시 업데이트하는 데 사용할 수 있는 동기 함수
+                - 두 번째 인자는 updater 함수인데 해당 함수의 첫 번째 매개변수는 oldData로 기존 데이터를 가져옴
+        */
+        queryClient.setQueryData(queryKey, newData);
+      }
+
+      return { oldData };
+    },
+    // NOTE: mutation이 실패하면 onMutate에서 반환된 context를 사용하여 롤백 진행
+    onError(
+      error,
+      variables,
+      context:
+        | {
+            oldData: [GetTotalLikeForRecordServerModel] | undefined;
+          }
+        | undefined,
+    ) {
+      queryClient.setQueryData(
+        bookRecordKeys.userRecordLike(variables.isbn, variables.recordId),
+        context?.oldData,
+      );
+    },
+    onSettled: (data, error, variables, context) => {
       queryClient.invalidateQueries({
         queryKey: bookRecordKeys.userRecordLike(
           variables.isbn,
@@ -98,7 +151,59 @@ export const useDeleteLikeForRecord = () => {
   return useMutation({
     mutationFn: (req: DeleteLikeForRecordQueryModel) =>
       deleteLikeForRecordAPI(req),
-    onSuccess: (_, variables) => {
+    onMutate: async (variables) => {
+      const queryKey = bookRecordKeys.userRecordLike(
+        variables.isbn,
+        variables.recordId,
+      );
+
+      // NOTE: 쿼리 취소
+      await queryClient.cancelQueries({ queryKey });
+
+      /*       
+        NOTE: getQueryData 함수
+              - 쿼리의 캐싱된 데이터를 가져오는 데 사용할 수 있는 동기 함수
+              - 쿼리가 존재하지 않으면 undefined를 반환
+      */
+      const oldData: [GetTotalLikeForRecordServerModel] | undefined =
+        queryClient.getQueryData(queryKey);
+
+      if (oldData && oldData.length > 0) {
+        const newData = [
+          {
+            isliked: !oldData[0].isliked,
+            count: oldData[0].isliked
+              ? oldData[0].count - 1
+              : oldData[0].count + 1,
+          },
+        ];
+
+        /* 
+          NOTE: setQueryData 함수
+                - 쿼리의 캐시 된 데이터를 즉시 업데이트하는 데 사용할 수 있는 동기 함수
+                - 두 번째 인자는 updater 함수인데 해당 함수의 첫 번째 매개변수는 oldData로 기존 데이터를 가져옴
+        */
+        queryClient.setQueryData(queryKey, newData);
+      }
+
+      return { oldData };
+    },
+    // NOTE: mutation이 실패하면 onMutate에서 반환된 context를 사용하여 롤백 진행
+    onError(
+      error,
+      variables,
+      context:
+        | {
+            oldData: [GetTotalLikeForRecordServerModel] | undefined;
+          }
+        | undefined,
+    ) {
+      queryClient.setQueryData(
+        bookRecordKeys.userRecordLike(variables.isbn, variables.recordId),
+        context?.oldData,
+      );
+    },
+    onSettled: (data, error, variables, context) => {
       queryClient.invalidateQueries({
         queryKey: bookRecordKeys.userRecordLike(
           variables.isbn,
