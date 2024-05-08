6d99c1ed75ff291e7c762ef0fcf32e115e53c177
SangWonLee <tkddnjs0687@naver.com>
2024-05-08 19:27:45 +0900

    feat: 메인 화면 베스트 독서 감상문 스켈레톤 UI 제작 (#29)

--- src/components/composites/main/bestCommentary/index.ts
+++ src/components/composites/main/bestCommentary/index.ts
@@ -1,1 +1,3 @@
 export { default as BestCommentary } from './BestCommentary';
+export { default as BestCommentaryDesktopSkeleton } from './desktop/BestCommentaryDesktop.skeleton';
+export { default as BestCommentaryMobileSkeleton } from './mobile/BestCommentaryMobile.skeleton';
--- src/components/composites/main/bestCommentary/mobile/BestCommentaryMobile.styled.ts
+++ src/components/composites/main/bestCommentary/mobile/BestCommentaryMobile.styled.ts
@@ -1,5 +1,12 @@
-import tw from 'twin.macro';
+import tw, { css, styled } from 'twin.macro';
 
 export const Title = tw.h2`ml-[24px] mb-[8px] m-body-m15`;
 
 export const swiper = tw`pb-[10px]`;
+
+export const SkeletonWrapper = styled.div`
+  & > span {
+    display: flex;
+    justify-content: center;
+  }
+`;
