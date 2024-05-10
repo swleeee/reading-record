41e7f3af89aeb138bffe4a92e9f6f3ecb814f647
SangWonLee <tkddnjs0687@naver.com>
2024-05-10 22:46:41 +0900

    feat: SettingDesktop, SettingMobile 파일들 Setting 파일로 이관 (#37)

--- src/components/composites/setting/Setting.tsx
+++ src/components/composites/setting/Setting.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 
+import { SideTab, Tab } from '@/components';
 import { deviceState } from '@/stores';
-import SettingDesktop from './SettingDesktop';
-import SettingMobile from './SettingMobile';
+import UserInfo from './userInfo/UserInfo';
+import * as S from './Setting.styled';
 
 const Setting = () => {
   const device = useRecoilValue(deviceState);
+  const tabs = [{ key: 'user', label: '회원 정보', content: <UserInfo /> }];
 
-  return device === 'mobile' ? <SettingMobile /> : <SettingDesktop />;
+  return device === 'mobile' ? (
+    <Tab tabs={tabs} />
+  ) : (
+    <S.Container>
+      <SideTab tabs={tabs} />
+    </S.Container>
+  );
 };
 
 export default Setting;
