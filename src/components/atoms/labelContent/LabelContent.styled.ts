1bc0ef6e1c1c0ca41e0502d3b4cd0b9f41115735
SangWonLee <tkddnjs0687@naver.com>
2024-05-14 19:51:36 +0900

    feat: LabelContent 컴포넌트 tooltip 항목 추가 관련 수정 (#40)

--- src/components/atoms/labelContent/LabelContent.styled.ts
+++ src/components/atoms/labelContent/LabelContent.styled.ts
@@ -2,13 +2,15 @@ import tw, { css, styled } from 'twin.macro';
 
 export const LabelContent = tw.div`flex flex-col`;
 
+export const LabelWrapper = tw.div`flex gap-x-[4px] mb-[2px]`;
+
 interface LabelProps {
   isRequired?: boolean;
 }
 
 export const Label = styled.label<LabelProps>`
   ${({ isRequired }) => css`
-    ${tw`relative flex items-center w-max mb-[2px] m-body-m13 text-gray800 tablet:t-body-m13 desktop:d-body-m15`}
+    ${tw`relative flex items-center m-body-m13 text-gray800 tablet:t-body-m13 desktop:d-body-m15`}
 
     &::after {
       ${isRequired && tw`content-["*"]`}
