64645c3eee4c62ac3ab8c75a9597545e68be8e2f
SangWonLee <tkddnjs0687@naver.com>
2024-05-15 14:52:39 +0900

    feat: Textarea disabled 상태 추가 관련 수정 (#40)

--- src/components/atoms/input/textarea/Textarea.styled.ts
+++ src/components/atoms/input/textarea/Textarea.styled.ts
@@ -5,13 +5,13 @@ export const TextareaWrapper = tw.div`relative`;
 export const Length = styled.span<{ hasValue: boolean }>`
   ${({ hasValue }) => css`
     ${tw`absolute top-[-12px] right-0 m-body-r13 -translate-y-1/2 tablet:t-body-r13 desktop:d-body-r15`}
-    ${hasValue ? tw`text-brown300` : tw`text-brown100`}
+    ${hasValue ? tw`text-gray300` : tw`text-gray100`}
   `}
 `;
 
 export const Textarea = styled.textarea<{ hasError?: boolean }>`
   ${({ hasError }) => css`
-    ${tw`w-full h-full border rounded-[4px] p-[12px] m-body-r12 bg-brown50 tablet:t-body-r12 desktop:d-body-r14`}
-    ${hasError ? tw`border-red200` : tw`border-brown300 `}
+    ${tw`w-full h-full border rounded-[4px] p-[12px] m-body-r12 disabled:(bg-gray50 cursor-not-allowed) tablet:t-body-r12 desktop:d-body-r14`}
+    ${hasError ? tw`border-red200` : tw`border-gray300 `}
   `}
 `;
