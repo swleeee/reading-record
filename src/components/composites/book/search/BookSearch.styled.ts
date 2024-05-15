42bf3a39c6ee359e759d0729cb3afc37c118b47c
SangWonLee <tkddnjs0687@naver.com>
2024-05-16 02:51:59 +0900

    feat: 도서 검색 Dropdown 영역 스타일 수정 (#40)

--- src/components/composites/book/search/BookSearch.styled.ts
+++ src/components/composites/book/search/BookSearch.styled.ts
@@ -3,7 +3,11 @@ import tw, { css } from 'twin.macro';
 export const BookSearchSection = tw.section`flex justify-center gap-x-[4px] mb-[64px]`;
 
 export const filterDropdown = css`
-  ${tw`w-[100px]`}
+  ${tw`w-[84px] tablet:w-[92px] desktop:w-[100px]`}
+
+  & > button {
+    ${tw`h-[44px]`}
+  }
 `;
 
 export const bookSearch = css`
