66d3748017ccb6cf3dc27f68f1c887de5b57b762
SangWonLee <tkddnjs0687@naver.com>
2024-05-15 14:55:44 +0900

    feat: 도서 읽기 상태 변경 시 key 변경 관련 수정 (#40)

--- src/apis/record.ts
+++ src/apis/record.ts
@@ -37,7 +37,7 @@ const createBookRecordPayload = (req: CreateBookRecordQueryModel) => {
   return {
     user_id: req.userId,
     isbn: req.isbn,
-    isPublic: req.isPublic,
+    public_flag: req.isPublic,
     rating: req.rating ?? null,
     reading_start_at: req.readingStartDate ?? null,
     reading_end_at: req.readingEndDate ?? null,
@@ -88,7 +88,7 @@ export const getBookUserRecordsAPI = async (
       'created_at, updated_at, id, isbn, rating, reading_start_at, reading_end_at, record_comment, like_count, user_id, users(nickname, profile_url)',
     )
     .eq('isbn', req.isbn)
-    .eq('isPublic', true)
+    .eq('public_flag', true)
     .not('rating', 'is', null)
     .not('reading_start_at', 'is', null)
     .not('reading_end_at', 'is', null)
@@ -191,6 +191,8 @@ export const deleteLikeForRecordAPI = async (
 };
 
 export const getBestRecordsAPI = async (req: GetBestRecordsQueryModel) => {
+  console.log(req);
+
   const { data, error } = await supabase
     .rpc('get_best_record', {
       current_user_id: req.userId,
