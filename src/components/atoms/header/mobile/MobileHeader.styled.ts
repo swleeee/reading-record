133cb4651cf863314e07b283be14497cbe8f3990
SangWonLee <tkddnjs0687@naver.com>
2024-05-16 19:58:21 +0900

    feat: Mobile Header 고정 (#42)

--- src/components/atoms/header/mobile/MobileHeader.styled.ts
+++ src/components/atoms/header/mobile/MobileHeader.styled.ts
@@ -1,6 +1,6 @@
 import tw, { css, styled } from 'twin.macro';
 
-export const MobileHeader = tw.div`relative flex justify-center items-center h-[50px] border-b border-b-gray-300`;
+export const MobileHeader = tw.div`sticky top-0 flex justify-center items-center h-[50px] border-b border-b-gray-300 bg-white z-HEADER`;
 
 export const logoLink = css`
   ${tw`m-headline-1 tablet:t-headline-1 labtop:d-headline-1`}
