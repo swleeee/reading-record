21f13717f158b840f859ac3ce48ad0a0451908b5
SangWonLee <tkddnjs0687@naver.com>
2024-05-25 13:28:59 +0900

    feat: 아이디/비밀번호 찾기 링크 -> 버튼 임시 수정 및 서비스 준비중 토스트 노출 처리 (#46)

--- src/components/composites/login/navigation/AuthNavigation.styled.ts
+++ src/components/composites/login/navigation/AuthNavigation.styled.ts
@@ -1,9 +1,5 @@
-import tw, { css } from 'twin.macro';
+import tw from 'twin.macro';
 
 export const AuthNavigation = tw.div`flex justify-between items-center w-full`;
-
-export const link = css`
-  ${tw`text-brown400 m-body-m14 tablet:t-body-m14 labtop:d-body-m14`};
-`;
 
 export const Division = tw.span`w-[1px] h-[12px] bg-brown400`;
