2f15bcede8e3add836abe7b6c07b18a60761d174
SangWonLee <tkddnjs0687@naver.com>
2024-06-04 22:55:36 +0900

    chore: 주석 삭제 (#19)

--- src/components/composites/login/navigation/AuthNavigation.tsx
+++ src/components/composites/login/navigation/AuthNavigation.tsx
@@ -5,7 +5,6 @@ import * as S from './AuthNavigation.styled';
 const AuthNavigation = () => {
   return (
     <S.AuthNavigation>
-      {/* TODO: 비밀번호 찾기 기능 구현 시 Button -> Link 변경 */}
       <Link sizeType="sm" styleType="tertiaryBrown" to={PATH.RESET_PASSWORD}>
         비밀번호 찾기
       </Link>
