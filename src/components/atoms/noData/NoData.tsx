642dee8350bb7224b0d7182aedbe095432497648
SangWonLee <tkddnjs0687@naver.com>
2024-06-12 23:45:26 +0900

    feat: NoData 컴포넌트 image prop으로 받아올 수 있도록 변경 (#56)

--- src/components/atoms/noData/NoData.tsx
+++ src/components/atoms/noData/NoData.tsx
@@ -1,15 +1,22 @@
+import { ElementType } from 'react';
+
 import NoDataIcon from '@/assets/icon/ic_error.svg?react';
 import * as S from './NoData.styled';
 
 interface NoDataProps {
   className?: string;
   content: string;
+  image?: ElementType;
 }
 
-const NoData = ({ className, content }: NoDataProps) => {
+const NoData = ({
+  className,
+  content,
+  image: Image = NoDataIcon,
+}: NoDataProps) => {
   return (
     <S.NoData className={className}>
-      <NoDataIcon css={S.noDataIcon} />
+      <Image css={S.noDataIcon} />
       <S.NoDataDescription>{content}</S.NoDataDescription>
     </S.NoData>
   );
