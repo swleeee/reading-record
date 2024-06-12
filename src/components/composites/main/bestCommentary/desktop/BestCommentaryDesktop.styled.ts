05163c191271fa3ad0cc939a604f9876fc5f4140
SangWonLee <tkddnjs0687@naver.com>
2024-06-12 23:46:16 +0900

    feat: 메인 화면 주간기록 없을 경우 스크린 제작 (#56)

--- src/components/composites/main/bestCommentary/desktop/BestCommentaryDesktop.styled.ts
+++ src/components/composites/main/bestCommentary/desktop/BestCommentaryDesktop.styled.ts
@@ -1,7 +1,11 @@
-import tw from 'twin.macro';
+import tw, { css } from 'twin.macro';
 
 export const BestCommentarySection = tw.section`tablet:mb-[64px] rounded-[4px] px-[24px] py-[16px] bg-white`;
 
 export const Title = tw.h2`tablet:t-body-m15 mb-[8px] desktop:d-body-m17`;
 
 export const BestCommentaryCardWrapper = tw.div`grid grid-cols-auto-fill-308 gap-x-[40px] gap-y-[20px]`;
+
+export const noRecordData = css`
+  ${tw`items-start`}
+`;
