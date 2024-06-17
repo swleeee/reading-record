34f26ee74f7ffa53d04b13586957faee494e487f
SangWonLee <tkddnjs0687@naver.com>
2024-06-17 22:42:36 +0900

    chore: 프로젝트 번들 시각화 적용 (#60)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 import * as path from 'path';
 
@@ -37,6 +38,11 @@ export default defineConfig({
     checker({
       typescript: true,
       eslint: { lintCommand: 'eslint ./src --ext .ts,.tsx' },
+    }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
     }),
   ],
   server: {
