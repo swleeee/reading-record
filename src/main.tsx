a3725559036d0ebfd1df98b68d73b3ea0cb3479a
SangWonLee <tkddnjs0687@naver.com>
2024-06-17 22:38:36 +0900

    chore: 스켈레톤 CSS 적용 위치 변경 (#60)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 import { AuthContextProvider } from './contexts';
 import { ModalPortal, Toast } from './components';
