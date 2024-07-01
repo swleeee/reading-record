233fcacab7687d6d2e36f95cb5b4d5d81b7795eb
SangWonLee <tkddnjs0687@naver.com>
2024-07-01 22:23:37 +0900

    storybook: 스토리북 Iconography 작성 (#68)

---
+++ src/stories/icon/IconItem.styled.ts
@@ -0,0 +1,7 @@
+import tw from 'twin.macro';
+
+export const Item = tw.div`flex flex-col items-center gap-y-[12px] w-[96px]`;
+
+export const IconWrapper = tw.div`flex justify-center items-center w-[24px] h-[24px] border border-light-grey-300 p-[6px] rounded-[4px] bg-white shadow-light_md`;
+
+export const Label = tw.span`m-body-m14 text-center tablet:t-body-m14 desktop:d-body-m16`;
